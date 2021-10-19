90ea0272c3387336bb7ccced415cfc248e069a7d
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-19 17:34:25 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const form = document.querySelector('form');
+const list = document.getElementById('tasks');
+const task = document.querySelector('#new-task-description');task.value
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
 });
+
+function createNewToDo(event){
+  event.preventDefault();
+  const newList = document.createElement('li');
+  newList.textContent = task.value;
+  
+  list.append(newList);
+  task.value = ''
+}
+
+document.addEventListener('submit', createNewToDo)
